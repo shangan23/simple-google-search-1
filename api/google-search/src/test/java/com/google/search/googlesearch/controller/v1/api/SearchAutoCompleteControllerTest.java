f0b6808563d8b4385c072fd59693b01c3f4f99e1
Shankar <shangan.23@gmail.com>
2020-12-19 23:01:06 +0530

    test(api/search/autocomplete): Base unit test setup

---
+++ api/google-search/src/test/java/com/google/search/googlesearch/controller/v1/api/SearchAutoCompleteControllerTest.java
@@ -0,0 +1,18 @@
+package com.google.search.googlesearch.controller.v1.api;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+class SearchAutoCompleteControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ api/google-search/src/test/java/com/google/search/googlesearch/services/SearchAutoCompleteServiceTest.java
@@ -0,0 +1,14 @@
+package com.google.search.googlesearch.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SearchAutoCompleteServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
