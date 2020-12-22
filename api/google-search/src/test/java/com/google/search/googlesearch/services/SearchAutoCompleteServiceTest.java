4e60502bb91c94fcd53c6d87310fabea1b37bc0c
Shankar <shangan.23@gmail.com>
2020-12-22 18:41:01 +0530

    test(searchAutoCompleteService): Added assertion for keyword "ama"

--- api/google-search/src/test/java/com/google/search/googlesearch/services/SearchAutoCompleteServiceTest.java
+++ api/google-search/src/test/java/com/google/search/googlesearch/services/SearchAutoCompleteServiceTest.java
@@ -2,13 +2,39 @@ package com.google.search.googlesearch.services;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 class SearchAutoCompleteServiceTest {
-
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSearchAutoCompleteService() {
+		ArrayList<String> results,expectedResults = new ArrayList<String>();
+		SearchAutoCompleteService searchAutoCompleteService = new SearchAutoCompleteService();
+		results = searchAutoCompleteService.findByKeywordUsingStream("ama");
+		expectedResults.add("amazon");
+		expectedResults.add("amazon prime");
+		expectedResults.add("amazon API");
+		expectedResults.add("amazon registry search");
+		expectedResults.add("amazon uk");
+		assertEquals(expectedResults,results);
+	}
+	
+	@Test
+	void testSearchAutoCompleteServiceForLoop() {
+		ArrayList<String> results,expectedResults = new ArrayList<String>();
+		SearchAutoCompleteService searchAutoCompleteService = new SearchAutoCompleteService();
+		results = searchAutoCompleteService.findByKeywordUsingLoop("ama");
+		expectedResults.add("amazon");
+		expectedResults.add("amazon prime");
+		expectedResults.add("amazon API");
+		expectedResults.add("amazon registry search");
+		expectedResults.add("amazon uk");
+		assertEquals(expectedResults,results);
 	}
 
 }
